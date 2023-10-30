38d666ee5e0c9834d272d3f4e14eaeaac0e39f9d
ZeyuGit <zyin@drew.edu>
2023-10-30 12:47:39 -0400

    Copied from Ephraim's branch

--- app.py
+++ app.py
@@ -1,50 +1,62 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+
 
 # Configure the SQLAlchemy database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://p3:Mtmqn584@34.136.218.122:3306/p3_courses"
 
+
 # Create a SQLAlchemy instance
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
 
 # Define a simple database model (optional)
-#class Course(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    name = db.Column(db.String(50))
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
 
-@app.route('/')
+
+
+@app.route('/', methods= ['GET', 'POST'])
 def home():
     return render_template('home.html')
 
-@app.route('/page1')
-def page1():
-    return render_template('page1.html')
+@app.route('/loginButtonP', methods= ['GET', 'POST'])
+def loginButtonP():
+    return render_template('homeP.html')
 
-@app.route('/page2')
-def page2():
-    return render_template('page2.html')
+
+@app.route('/seeHistoryButtonP', methods= ['GET', 'POST'])
+def seeHistoryButtonP():
+    return render_template('seeHistoryP.html')
+
+@app.route('/applyButtonP', methods= ['GET', 'POST'])
+def applyButtonP():
+    return render_template('applyP.html')
+
+
+@app.route('/loginP')
+def loginP():
+    return render_template('loginP.html')
+
+
+@app.route('/loginButtonA')
+def loginButtonA():
+    return render_template('loginA.html')
+
+
+@app.route('/loginA')
+def loginA():
+    return render_template('homeA.html')
+
 
 @app.route('/page3')
 def page3():
     return render_template('page3.html')
 
-@app.route('/history')
-def see_historyPG():
-    return render_template('see_historyPG.html')
-
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
-
-@app.route('/')
-def LoginAdmin():
-    return render_template('LoginAdmin.html')
-
-@app.route('/')
-def LoginProf():
-    return render_template('LoginProf.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
